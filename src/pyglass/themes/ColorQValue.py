0d3ac849d744c96900d2ef9aa64a1394e875863f
sernst <swernst@gmail.com>
2013-04-06 16:17:23 -0700

    Add color classes for theme support.

---
+++ src/pyglass/themes/ColorQValue.py
@@ -0,0 +1,30 @@
+# ColorQValue.py
+# (C)2013
+# Scott Ernst
+
+from PySide import QtGui
+
+from pyaid.color.ColorValue import ColorValue
+
+#___________________________________________________________________________________________________ ColorQValue
+class ColorQValue(ColorValue):
+    """A class for..."""
+
+#===================================================================================================
+#                                                                                       C L A S S
+
+#___________________________________________________________________________________________________ __init__
+    def __init__(self, sourceColor, normalized, opacity):
+        """Creates a new instance of ColorQValue."""
+        super(ColorQValue, self).__init__(sourceColor, normalized, opacity)
+
+#===================================================================================================
+#                                                                                   G E T / S E T
+
+#___________________________________________________________________________________________________ GS: qColor
+    @property
+    def qColor(self):
+        color = QtGui.QColor.fromRgba(self._rawColor)
+        color.setAlphaF(self._opacity)
+        return color
+
